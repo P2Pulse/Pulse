527120cf231b004a17861cd88adef4a5f2eeaa5e
Gur Galler <gur@kissterra.com>
2022-04-23 14:49:15 +0300

    Added missing file

---
+++ Pulse.Core/Connections/PortBruteForceNatTraversal.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Pulse.Core.Connections;
+
+internal class PortBruteForceNatTraversal : IConnectionEstablishmentStrategy
+{
+    private const int SourcePort = 54551;
+
+    public async Task<IConnection> EstablishConnectionAsync(IPAddress destination, 
+        CancellationToken cancellationToken = default)
+    {
+        var shouldSend = true;
+        using var client = new UdpClient(SourcePort);
+        _ = client.ReceiveAsync(cancellationToken)
+            .AsTask().ContinueWith(task =>
+            {
+                shouldSend = false;
+                Console.WriteLine("Message received: " + Encoding.ASCII.GetString(task.Result.Buffer));
+            }, cancellationToken);
+
+        for (var i = 0; shouldSend; i++)
+        {
+            await destination.SendMessageAsync(client, i % ushort.MaxValue);
+        }
+
+        return null;
+    }
+}
