360f443694069dd5d74e8a4ecacd2080f1ee0b26
Gur Galler <gallergur@gmail.com>
2022-08-06 12:29:07 +0300

    Some changes:)

--- Pulse.Core/Connections/UdpMultiHolePunching.cs
+++ Pulse.Core/Connections/UdpMultiHolePunching.cs
@@ -19,7 +19,7 @@ public class UdpMultiHolePunching : IConnectionEstablishmentStrategy
             Console.WriteLine("Tell the echo server my port is around " + predictedNextPort);
             Console.Write("What is the server's port? ");
             var serverPort = int.Parse(Console.ReadLine()!);
-            await Parallel.ForEachAsync(Enumerable.Repeat(0, 200), cancellationToken, async (_, ct) =>
+            await Parallel.ForEachAsync(Enumerable.Repeat(0, 1000), cancellationToken, async (_, ct) =>
             {
                 var receiver = new UdpClient
                 {
@@ -63,7 +63,7 @@ public class UdpMultiHolePunching : IConnectionEstablishmentStrategy
             Console.WriteLine("What is the client's port? ");
             var clientPort = int.Parse(Console.ReadLine()!);
             // Hole punching
-            await Parallel.ForEachAsync(Enumerable.Repeat(0, 200), cancellationToken, async (_, ct) =>
+            await Parallel.ForEachAsync(Enumerable.Repeat(0, 1000),new ParallelOptions{MaxDegreeOfParallelism = 1000}, async (_, ct) =>
             {
                 var udpClient = new UdpClient
                 {
@@ -102,7 +102,11 @@ public class UdpMultiHolePunching : IConnectionEstablishmentStrategy
                 sender.Client.Bind(udpClient.Client.LocalEndPoint);
                 sender.Ttl = 5;
                 var punch = Encoding.ASCII.GetBytes("In your face, NAT!");
-                await sender.SendAsync(punch,new IPEndPoint(destination, clientPort), ct);
+                while (true)
+                {
+                    await sender.SendAsync(punch,new IPEndPoint(destination, clientPort), ct);
+                    await Task.Delay(1000, ct);
+                }
             });
             Console.WriteLine("Tell the client my port is around " + predictedNextPort);
         }
@@ -113,12 +117,21 @@ public class UdpMultiHolePunching : IConnectionEstablishmentStrategy
 
     private static async Task<int> PredictNextPortAsync(CancellationToken cancellationToken)
     {
-        var s1 = "stun.schlund.de";
-        var s2 = "stun.jumblo.com";
-        var s1Response = await GetPublicIPEndpointAsync(s1, cancellationToken);
-        var s2Response = await GetPublicIPEndpointAsync(s2, cancellationToken);
+        var result = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            var s1 = "stun.schlund.de";
+            var s2 = "stun.jumblo.com";
+            var s1Response = await GetPublicIPEndpointAsync(s1, cancellationToken);
+            var s2Response = await GetPublicIPEndpointAsync(s2, cancellationToken);
 
-        return 2 * s2Response.Port - s1Response.Port;
+            Console.WriteLine($"{s1} is {s1Response.Port}");
+            Console.WriteLine($"{s2} is {s2Response.Port}");
+        
+            result = 2 * s2Response.Port - s1Response.Port;
+        }
+
+        return result;
     }
 
     private static async Task<IPEndPoint> GetPublicIPEndpointAsync(string hostName,
