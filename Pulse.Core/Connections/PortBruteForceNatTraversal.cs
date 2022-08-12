aaa672230a7d2462489dd7b554470cbb24fe6624
yotams <yotam@idanimclinic.co.il>
2022-08-12 13:12:16 +0300

    Its time to admit python was the best choice for this project.

--- Pulse.Core/Connections/PortBruteForceNatTraversal.cs
+++ Pulse.Core/Connections/PortBruteForceNatTraversal.cs
@@ -1,15 +1,13 @@
-﻿using System.Diagnostics;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using STUN;
 
 namespace Pulse.Core.Connections;
 
 public class PortBruteForceNatTraversal : IConnectionEstablishmentStrategy
 {
-    private const int SourcePort = 54911;
-
-    public async Task<IConnection> EstablishConnectionAsync(IPAddress destination, 
+    public async Task<IConnection> EstablishConnectionAsync(IPAddress destination,
         CancellationToken cancellationToken = default)
     {
         var receiver = new UdpClient
@@ -17,7 +15,7 @@ public class PortBruteForceNatTraversal : IConnectionEstablishmentStrategy
             ExclusiveAddressUse = false
         };
         receiver.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-        receiver.Client.Bind(new IPEndPoint(IPAddress.Any, 0)); 
+        receiver.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
         ThreadPool.QueueUserWorkItem(async _ =>
         {
             using (receiver)
@@ -35,58 +33,65 @@ public class PortBruteForceNatTraversal : IConnectionEstablishmentStrategy
                         // throw;
                         return;
                     }
+
                     Console.WriteLine($"Received {message.Buffer.Length} bytes from {message.RemoteEndPoint}:");
                     var messageContent = Encoding.ASCII.GetString(message.Buffer);
                     Console.WriteLine(messageContent);
                 }
+
                 Console.WriteLine("juyhtgf");
             }
         });
-                
+
         using var sender = new UdpClient();
         sender.ExclusiveAddressUse = false;
         sender.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-        sender.Client.Bind(receiver.Client.LocalEndPoint);
-        var port = ((IPEndPoint)receiver.Client.LocalEndPoint).Port;
-        var message = Encoding.ASCII.GetBytes($"Hello, I'm talking from {port}");
-        for (var i = 54448;; i++)
-        {
-            var endpoint = new IPEndPoint(destination, i % ushort.MaxValue + 1);
-            await sender.SendAsync(message, message.Length, endpoint);
-            if (i % 10 is 0)
-                await Task.Delay(1, cancellationToken);
-        }
-
-        return null;
-        /*var shouldSend = true;
-        var destinationPort = 0;
-        using var client = new UdpClient(SourcePort);
-        _ = client.ReceiveAsync(cancellationToken)
-            .AsTask().ContinueWith(task =>
-            {
-                Console.WriteLine("hey got message");
-                var udpReceiveResult = task.Result;
-                destinationPort = udpReceiveResult.RemoteEndPoint.Port;
-                Console.WriteLine("Message received: " + Encoding.ASCII.GetString(udpReceiveResult.Buffer));
-                shouldSend = false;
-            }, cancellationToken);
-
-        for (var i = 0; shouldSend; i++)
-        {
-            try
-            {
-                await destination.SendMessageAsync(client, i % ushort.MaxValue);
-            }
-            catch (Exception e)
-            {
-            }
-        }
-
+        sender.Client.Bind(receiver.Client.LocalEndPoint!);
+        var port = ((IPEndPoint)receiver.Client.LocalEndPoint!).Port;
+        
+        Console.WriteLine(await PredictMinMaxPortsAsync(cancellationToken));
+        Console.WriteLine("what is minimum port of the other person?: ");
+        var minPort = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("what is maximum port of the other person?: ");
+        var maxPort = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Starting");
         while (true)
         {
-            await destination.SendMessageAsync(client, destinationPort, message: "Yayy");
+            for (var destinationPort = minPort; destinationPort <= maxPort; destinationPort++)
+            {
+                var endpoint = new IPEndPoint(destination, destinationPort);
+                var message = Encoding.ASCII.GetBytes($"Hey from {port} sent to {destinationPort}");
+                await sender.SendAsync(message, message.Length, endpoint);
+                if (destinationPort % 10 is 0)
+                    await Task.Delay(1, cancellationToken);
+            }
+            
+            await Task.Delay(1232, cancellationToken);
+            Console.WriteLine("loop");
         }
-        // TODO - should listen to the UdpPort and wait for an answer from the other person
-        return null;*/
     }
+    private static async Task<IPEndPoint> GetPublicIPEndpointAsync(string hostName,
+        CancellationToken cancellationToken)
+    {
+        var serverIp = (await Dns.GetHostAddressesAsync(hostName, cancellationToken)).First();
+        var server = new IPEndPoint(serverIp, 3478);
+        var result = await STUNClient.QueryAsync(server, STUNQueryType.PublicIP, closeSocket: true);
+        return result.PublicEndPoint;
+    }
+   
+    private static async Task<(int, int)> PredictMinMaxPortsAsync(CancellationToken cancellationToken)
+    {
+        var s1 = "stun.schlund.de";
+        var s2 = "stun.jumblo.com";
+        var stunQueriesS1 = Enumerable.Range(0, 50).Select(i => GetPublicIPEndpointAsync(s1, cancellationToken));
+        var stunQueriesS2 = Enumerable.Range(0, 50).Select(i => GetPublicIPEndpointAsync(s2, cancellationToken));
+        var responses = await Task.WhenAll(stunQueriesS1.Concat(stunQueriesS2));
+        var ports = responses.Select(r => r.Port).ToList();
+        var max = ports.Max();
+        var min = ports.Min();
+        min = Math.Max(min - 100, 1);
+        max = Math.Min(max + 100, ushort.MaxValue);
+        return (min, max);
+    }
+
 }
