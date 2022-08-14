b2ab348799d0cdadc64a82102422fd38daa7eb07
yotams <yotam@idanimclinic.co.il>
2022-08-14 22:26:37 +0300

    Git is better than good docs

--- Pulse.Core/Connections/UdpBiDirectionalStream.cs
+++ Pulse.Core/Connections/UdpBiDirectionalStream.cs
@@ -6,28 +6,42 @@ internal class UdpBiDirectionalStream : IBiDirectionalStream
 {
     private readonly UdpClient sender;
     private readonly UdpClient receiver;
+
     public UdpBiDirectionalStream(Socket socket)
     {
+        var socketLocalEndPoint = socket.LocalEndPoint;
+        var socketRemoteEndPoint = socket.RemoteEndPoint;
+        socket.Dispose();
+        
         sender = new UdpClient
         {
-            Client = socket
+            ExclusiveAddressUse = false
         };
+        
+        sender.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        sender.Client.Bind(socketLocalEndPoint!);
+        sender.Client.Connect(socketRemoteEndPoint!);
+        
         receiver = new UdpClient
         {
-            Client = socket
+            ExclusiveAddressUse = false
         };
+        receiver.Client.Bind(socketLocalEndPoint!);
+        
+        sender.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        
         Input = new UdpStream(receiver, isReader: true);
         Output = new UdpStream(sender, isReader: false);
     }
-    
+
     public Stream Input { get; }
     public Stream Output { get; }
-    
+
     public ValueTask DisposeAsync()
     {
         sender.Dispose();
         receiver.Dispose();
-        
+
         return ValueTask.CompletedTask;
     }
 }
