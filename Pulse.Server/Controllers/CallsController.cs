009159dd8857ea61ef16a4200f43541f8575f602
Gur Galler <gallergur@gmail.com>
2022-08-19 14:27:20 +0300

    Added call matching functionality to the server

---
+++ Pulse.Server/Controllers/CallsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Pulse.Server.Core;
+
+namespace Pulse.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class CallsController : ControllerBase
+{
+    private readonly InMemoryCallMatcher callMatcher;
+    private readonly UserManager<IdentityUser> userManager;
+
+    public CallsController(InMemoryCallMatcher callMatcher, UserManager<IdentityUser> userManager)
+    {
+        this.callMatcher = callMatcher;
+        this.userManager = userManager;
+    }
+    
+    /// <summary>
+    /// Initiates a new call
+    /// </summary>
+    /// <param name="request">Details about the call</param>
+    /// <returns>Connection details</returns>
+    [HttpPost]
+    public async Task<ActionResult<ConnectionDetails>> InitiateNewCallAsync([FromBody] InitiateCallRequest request)
+    {
+        return await callMatcher.InitiateCallAsync(request, HttpContext.Connection.RemoteIpAddress!);
+    }
+    
+    /// <summary>
+    /// Polls for incoming calls
+    /// </summary>
+    /// <returns>True if there is an awaiting incoming call, False otherwise</returns>
+    [HttpGet]
+    public ActionResult<bool> PollForIncomingCallAsync()
+    {
+        var userName = User.Identities.First().Name;
+        return callMatcher.PollForIncomingCall(userName);
+    }
+    
+    /// <summary>
+    /// Accepts an incoming call
+    /// </summary>
+    /// <param name="request">Details about how to connect the call</param>
+    /// <returns>Connection details</returns>
+    [HttpPost("accept")]
+    public ActionResult<ConnectionDetails> AcceptIncomingCallAsync([FromBody] AcceptCallRequest request)
+    {
+        var userName = User.Identities.First().Name;
+        return callMatcher.AcceptIncomingCall(request, userName, HttpContext.Connection.RemoteIpAddress!.ToString());
+    }
+}
--- Pulse.Server/Program.cs
+++ Pulse.Server/Program.cs
@@ -1,15 +1,54 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Reflection;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
+using Pulse.Server.Conventions;
 using Pulse.Server.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    var signingKey = builder.Configuration["Authentication:SecretKey"];
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+    };
+});
 
 builder.Services.AddSingleton(new MongoClient(builder.Configuration.GetConnectionString("MainMongoDBConnection")));
 
@@ -26,6 +65,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
     .AddRoleStore<FakeRoleStore>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
