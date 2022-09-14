bf76d82163f54e19623c79377a40bcc045481ba6
NatalieShaked <shakednatalie@gmail.com>
2022-09-14 23:41:02 +0300

    added the blazorHybridApp

---
+++ FirstBlazorHybridApp/FirstBlazorHybridApp/CounterState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstBlazorHybridApp
+{
+    internal class CounterState
+    {
+        public int CurrentCount { get; private set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+            StateChanged?.Invoke();
+        }
+
+        public event Action StateChanged;
+    }
+}
