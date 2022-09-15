b3a557e2aaf66b8f3932eb8a0479f0a2363ac4bb
yotams <yotam@idanimclinic.co.il>
2022-09-16 01:07:36 +0300

    Encrypting packets.

--- Pulse.Core/PacketEncryptor.cs
+++ Pulse.Core/PacketEncryptor.cs
@@ -1,9 +1,61 @@
 using System.Security.Cryptography;
+using System.Threading.Channels;
 using Pulse.Core.Connections;
 
 namespace Pulse.Core;
 
-internal class PacketEncryptor : IDisposable
+internal class EncryptedConnection : IConnection
+{
+    private readonly IConnection actualConnection;
+    private readonly PacketEncryptor encryptor;
+    public ChannelReader<Packet> IncomingAudio { get; }
+
+    public EncryptedConnection(IConnection actualConnection, PacketEncryptor encryptor)
+    {
+        if (!encryptor.IsSharedKeySet)
+        {
+            throw new InvalidOperationException("Shared key is not set");
+        }
+
+        this.actualConnection = actualConnection;
+        this.encryptor = encryptor;
+        IncomingAudio = new CryptoPacketChannelReader(this.actualConnection.IncomingAudio, encryptor);
+    }
+
+    public async Task SendPacketAsync(Packet packet, CancellationToken cancellationToken)
+    {
+        var encryptedPacket = await encryptor.EncryptAsync(packet);
+        await actualConnection.SendPacketAsync(encryptedPacket, cancellationToken);
+    }
+
+    private class CryptoPacketChannelReader : ChannelReader<Packet>
+    {
+        private readonly ChannelReader<Packet> actualChannelReader;
+        private readonly PacketEncryptor encryptor;
+
+        public CryptoPacketChannelReader(ChannelReader<Packet> actualChannelReader, PacketEncryptor encryptor)
+        {
+            this.actualChannelReader = actualChannelReader;
+            this.encryptor = encryptor;
+        }
+
+        public override bool TryRead(out Packet item)
+        {
+            if (!actualChannelReader.TryRead(out item))
+                return false;
+
+            item = encryptor.DecryptAsync(item).GetAwaiter().GetResult();  // Todo make the encrypt and decrypt methods sync
+            return true;
+        }
+
+        public override ValueTask<bool> WaitToReadAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return actualChannelReader.WaitToReadAsync(cancellationToken);
+        }
+    }
+}
+
+internal class PacketEncryptor : IDisposable // rename me todo
 {
     private readonly byte[] aesIV;
     private readonly ECDiffieHellman ecDiffieHellman;
@@ -27,6 +79,8 @@ internal class PacketEncryptor : IDisposable
         sharedKey = ecDiffieHellman.DeriveKeyMaterial(otherPartyECDH.PublicKey);
     }
 
+    public bool IsSharedKeySet => sharedKey != null;
+
     public async Task<Packet> EncryptAsync(Packet packet)
     {
         if (sharedKey == null)
@@ -37,7 +91,7 @@ internal class PacketEncryptor : IDisposable
         using var aes = Aes.Create();
         aes.Key = sharedKey;
         aes.IV = iv;
-        
+
         var encryptor = aes.CreateEncryptor();
 
         var encryptedContent = await CryptoTransformAsync(packet.Content, encryptor);
@@ -54,14 +108,15 @@ internal class PacketEncryptor : IDisposable
         using var aes = Aes.Create();
         aes.Key = sharedKey;
         aes.IV = iv;
-        
+
         var decryptor = aes.CreateDecryptor();
 
         var decryptedContent = await CryptoTransformAsync(encryptedPacket.Content, decryptor);
-        return encryptedPacket with { Content = decryptedContent.ToArray()}; 
+        return encryptedPacket with { Content = decryptedContent.ToArray() };
     }
 
-    private static async Task<MemoryStream> CryptoTransformAsync(ReadOnlyMemory<byte> data, ICryptoTransform cryptoTransformer)
+    private static async Task<MemoryStream> CryptoTransformAsync(ReadOnlyMemory<byte> data,
+        ICryptoTransform cryptoTransformer)
     {
         var dataStream = new MemoryStream();
         await using var cs = new CryptoStream(dataStream, cryptoTransformer, CryptoStreamMode.Write);
