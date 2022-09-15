f6e59adbc69b6aec8a302f2e943bc12b44735fce
yotams <yotam@idanimclinic.co.il>
2022-09-16 01:24:20 +0300

    Refactoring

--- Pulse.Core/PacketEncryptor.cs
+++ Pulse.Core/PacketEncryptor.cs
@@ -1,59 +1,7 @@
 using System.Security.Cryptography;
-using System.Threading.Channels;
 using Pulse.Core.Connections;
 
 namespace Pulse.Core;
-
-internal class EncryptedConnection : IConnection
-{
-    private readonly IConnection actualConnection;
-    private readonly PacketEncryptor encryptor;
-    public ChannelReader<Packet> IncomingAudio { get; }
-
-    public EncryptedConnection(IConnection actualConnection, PacketEncryptor encryptor)
-    {
-        if (!encryptor.IsSharedKeySet)
-        {
-            throw new InvalidOperationException("Shared key is not set");
-        }
-
-        this.actualConnection = actualConnection;
-        this.encryptor = encryptor;
-        IncomingAudio = new CryptoPacketChannelReader(this.actualConnection.IncomingAudio, encryptor);
-    }
-
-    public async Task SendPacketAsync(Packet packet, CancellationToken cancellationToken)
-    {
-        var encryptedPacket = await encryptor.EncryptAsync(packet);
-        await actualConnection.SendPacketAsync(encryptedPacket, cancellationToken);
-    }
-
-    private class CryptoPacketChannelReader : ChannelReader<Packet>
-    {
-        private readonly ChannelReader<Packet> actualChannelReader;
-        private readonly PacketEncryptor encryptor;
-
-        public CryptoPacketChannelReader(ChannelReader<Packet> actualChannelReader, PacketEncryptor encryptor)
-        {
-            this.actualChannelReader = actualChannelReader;
-            this.encryptor = encryptor;
-        }
-
-        public override bool TryRead(out Packet item)
-        {
-            if (!actualChannelReader.TryRead(out item))
-                return false;
-
-            item = encryptor.DecryptAsync(item).GetAwaiter().GetResult();  // Todo make the encrypt and decrypt methods sync
-            return true;
-        }
-
-        public override ValueTask<bool> WaitToReadAsync(CancellationToken cancellationToken = new CancellationToken())
-        {
-            return actualChannelReader.WaitToReadAsync(cancellationToken);
-        }
-    }
-}
 
 internal class PacketEncryptor : IDisposable
 {
