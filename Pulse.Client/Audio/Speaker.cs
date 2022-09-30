1bb2340e2460314518e9ee4c5e2f1584e3778a57
yotams <yotam@idanimclinic.co.il>
2022-09-30 15:13:04 +0300

    Using obsolete constructors is not the best practice

--- Pulse.Client/Audio/Speaker.cs
+++ Pulse.Client/Audio/Speaker.cs
@@ -9,9 +9,15 @@ public class Speaker
     public async Task PlayAsync(Stream stream)
     {
         var buffer = new byte[320];
+        var audioAttributes = new AudioAttributes.Builder().SetContentType(AudioContentType.Speech)?.SetUsage(AudioUsageKind.VoiceCommunication)?.Build();
+        var audioFormat = new AudioFormat.Builder()
+            .SetEncoding(Encoding.Pcm16bit)?
+            .SetSampleRate(16_000)?
+            .SetChannelMask(ChannelOut.Mono).Build();
 
-        var audioTrack = new AudioTrack(AndroidStream.VoiceCall, sampleRateInHz: 16_000, ChannelOut.Mono, Encoding.Pcm16bit,
-            buffer.Length, AudioTrackMode.Stream);
+        var audioTrack = new AudioTrack(audioAttributes, audioFormat, bufferSizeInBytes: buffer.Length, AudioTrackMode.Stream, 0);
+        // var audioTrack = new AudioTrack(AndroidStream.VoiceCall, sampleRateInHz: 16_000, ChannelOut.Mono, Encoding.Pcm16bit,
+            // buffer.Length, AudioTrackMode.Stream);
 
         audioTrack.Play();
 
