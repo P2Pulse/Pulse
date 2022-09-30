6448c99e94d68600e4900b1451194833482bd1fb
yotams <yotam@idanimclinic.co.il>
2022-09-30 15:52:45 +0300

    Echo cancellation

--- Pulse.Client/Audio/Microphone.cs
+++ Pulse.Client/Audio/Microphone.cs
@@ -1,4 +1,5 @@
 using Android.Media;
+using Android.Media.Audiofx;
 using AndroidStream = Android.Media.Stream;
 using Stream = System.IO.Stream;
 
@@ -6,15 +7,23 @@ namespace Pulse.Client.Audio;
 
 public class Microphone
 {
+    private readonly AudioRecord recorder;
+    private const int BufferLength = 320;
+
+    public int audioSessionId { get; }
+
+    public Microphone()
+    {
+        recorder = new AudioRecord(AudioSource.VoiceCommunication, sampleRateInHz: 16_000, ChannelIn.Mono,
+            Encoding.Pcm16bit, BufferLength);
+        audioSessionId = recorder.AudioSessionId;
+    }
+
+
     public async Task RecordAsync(Stream streamToWriteTo, CancellationToken ct)
     {
-        var buffer = new byte[320];
-        
-        var recorder = new AudioRecord(AudioSource.VoiceCommunication, sampleRateInHz: 16_000, ChannelIn.Mono,
-            Encoding.Pcm16bit, buffer.Length);
-        
         recorder.StartRecording();
-        
+        var buffer = new byte[BufferLength];
         while (!ct.IsCancellationRequested)
         {
             var read = await recorder.ReadAsync(buffer, 0, buffer.Length);
--- Pulse.Client/Audio/Speaker.cs
+++ Pulse.Client/Audio/Speaker.cs
@@ -1,4 +1,5 @@
 using Android.Media;
+using Android.Media.Audiofx;
 using AndroidStream = Android.Media.Stream;
 using Stream = System.IO.Stream;
 
@@ -6,21 +7,23 @@ namespace Pulse.Client.Audio;
 
 public class Speaker
 {
-    public async Task PlayAsync(Stream stream)
+    private const int BufferLength = 320;
+
+    public async Task PlayAsync(Stream stream, int sessionId)
     {
-        var buffer = new byte[320];
         var audioAttributes = new AudioAttributes.Builder().SetContentType(AudioContentType.Speech)?.SetUsage(AudioUsageKind.VoiceCommunication)?.Build();
         var audioFormat = new AudioFormat.Builder()
             .SetEncoding(Encoding.Pcm16bit)?
             .SetSampleRate(16_000)?
             .SetChannelMask(ChannelOut.Mono).Build();
 
-        var audioTrack = new AudioTrack(audioAttributes, audioFormat, bufferSizeInBytes: buffer.Length, AudioTrackMode.Stream, 0);
-        // var audioTrack = new AudioTrack(AndroidStream.VoiceCall, sampleRateInHz: 16_000, ChannelOut.Mono, Encoding.Pcm16bit,
-            // buffer.Length, AudioTrackMode.Stream);
-
+        var audioTrack = new AudioTrack(audioAttributes, audioFormat, bufferSizeInBytes: BufferLength, AudioTrackMode.Stream, sessionId);
+        var buffer = new byte[BufferLength];
         audioTrack.Play();
-
+        
+        var aec = AcousticEchoCanceler.Create(sessionId);
+        aec.SetEnabled(true);
+        
         int bytesRead;
         while ((bytesRead = await stream.ReadAsync(buffer)) != 0)
             await audioTrack.WriteAsync(buffer, 0, bytesRead);
